550aea03550e69997f40ac6c7feecbe5fc01ee3e
amirrajj <amiramraja@gmail.com>
2025-09-17 17:04:41 +0330

    env validation with joi🕷️🖤

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { CommentsModule } from './comments/comments.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { PrismaModule } from './prisma/prisma.module';
+import { LoggerModule } from './common/logger/logger.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        NODE_ENV: Joi.string().required().valid('development', 'production'),
+        SALT: Joi.number().required().default(10),
+        ALLOWED_EMAIL_DOMAINS : Joi.string().required().default('gmail.com')
+      }),
+    }),
+    PrismaModule,
+    LoggerModule,
     AuthModule,
     UsersModule,
     ProjectsModule,
@@ -19,6 +34,6 @@ import { NotificationsModule } from './notifications/notifications.module';
     NotificationsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
