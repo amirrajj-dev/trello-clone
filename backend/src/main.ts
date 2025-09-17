a582d0d7a68413f46cc3d65ac4704340d263d7d5
amirrajj <amiramraja@gmail.com>
2025-09-17 17:03:58 +0330

    add some global configurations🕷️🖤

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/filters/http-exceptiot.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const NODE_ENV = configService.get<string>('NODE_ENV');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      enableDebugMessages: NODE_ENV === 'development',
+      whitelist: true,
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
