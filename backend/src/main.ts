8ee29817cc5791c017d3be5e99d8ea7de898a460
amirrajj <amiramraja@gmail.com>
2025-09-19 13:42:44 +0330

    update entry point file transform options🕷️🖤

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './common/filters/http-exceptiot.filter';
+import { ResponseInterceptor } from './common/intercepters/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,9 +14,13 @@ async function bootstrap() {
       transform: true,
       enableDebugMessages: NODE_ENV === 'development',
       whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true, // for using transform for dtos
+      },
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
