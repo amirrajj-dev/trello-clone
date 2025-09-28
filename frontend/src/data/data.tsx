0a50e9233996d4a9eb7c1a4bcc586ed7efe04d54
amirrajj <amiramraja@gmail.com>
2025-09-28 23:58:18 +0330

    added roleStyles to data file🕷️🖤

--- frontend/src/data/data.tsx
+++ frontend/src/data/data.tsx
@@ -1,3 +1,5 @@
+import { Crown, Eye, Shield, User } from "lucide-react";
+
 type Theme = "light" | "dark" | "night" | "forest" | "dracula" | "coffee";
 
 export const themeItems: {
@@ -10,3 +12,10 @@ export const themeItems: {
   { value: "dracula" },
   { value: "coffee" },
 ];
+
+  export const roleStyles = {
+    OWNER: <Crown size={12} className="text-amber-500" />,
+    ADMIN: <Shield size={12} className="text-blue-500" />,
+    MEMBER: <User size={12} className="text-green-500" />,
+    VIEWER: <Eye size={12} className="text-gray-500" />,
+  };
