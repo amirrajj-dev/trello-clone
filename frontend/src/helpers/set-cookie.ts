fc9c4dbb192af5d6079b358656d25ea3a9e003bd
amirrajj <amiramraja@gmail.com>
2025-10-01 14:02:12 +0330

    fixed a cookie setting problem🕷️🖤

--- frontend/src/helpers/set-cookie.ts
+++ frontend/src/helpers/set-cookie.ts
@@ -4,12 +4,14 @@ import { cookies } from "next/headers";
 
 export const setCookie = async (token: string, maxAge: number) => {
   const cookieStore = await cookies();
+  const isProduction = process.env.NODE_ENV === "production";
+  
   if (token) {
     cookieStore.set("trello-token", token, {
       httpOnly: true,
       path: "/",
-      secure: true,
-      sameSite: "strict",
+      secure: isProduction,
+      sameSite: isProduction ? "lax" : "lax", 
       maxAge,
     });
   }
